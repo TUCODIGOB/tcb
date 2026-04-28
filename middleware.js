50cffc9e7da6be9f64ad5c65ee787aa2e3439850
TUCODIGOB <hola@tucodigobase.com>
2026-04-28 08:02:34 +0200

    Delete middleware.js

--- middleware.js
+++
@@ -1,8 +0,0 @@
-export function middleware(request) {
-  const url = request.nextUrl.clone();
-  const lower = url.pathname.toLowerCase();
-  if (lower !== url.pathname) {
-    url.pathname = lower;
-    return Response.redirect(url, 301);
-  }
-}
